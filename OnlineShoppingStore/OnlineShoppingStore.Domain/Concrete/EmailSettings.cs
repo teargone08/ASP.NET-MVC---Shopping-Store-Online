93c60068d3695df3fc2ce759fa82db6a8e7f0c0c
teargone08 <teargone08@yahoo.com>
2016-01-28 23:19:22 -0600

    final001
    version control

---
+++ OnlineShoppingStore/OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "tonytong003@gmail.com";
+        public string MailFromAddress = "tonytong003@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "tonytong003@gmail.com";
+        public string Password = "StoreProject";
+        public string ServerName="smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
---
+++ OnlineShoppingStore/OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly IProductRepository repository;
+        public int PageSize=4; 
+        public ProductController(IProductRepository repo)
+        {
+            this.repository = repo;
+        }
+        public ActionResult List(string category,int page=1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel()
+            {
+                Products=repository.Products
+                .Where(p=>category==null||p.Category==category)
+                .OrderBy(p=>p.ProductId).Skip((page-1)*PageSize).Take(PageSize),
+                PagingInfo=new PagingInfo(){
+                    CurrentPage=page,
+                    ItemsPerPage=PageSize,
+                   // TotalItems=repository.Products.Count()
+                   TotalItems=category==null? 
+                   repository.Products.Count():repository.Products.Where(p=>p.Category==category).Count()
+                },
+                CurrentCategory=category
+            };
+            return View(model);
+        }
+    }
+}
